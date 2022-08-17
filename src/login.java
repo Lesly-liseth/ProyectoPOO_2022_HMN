0d7b3b73f348bfe96efa7fc7ce61015efef53551
Lesly <leslyherrera851@gmail.com>
2022-08-17 18:58:34 -0500

    funcionalidad de login
    se le dio funcionalidad al login

--- src/login.java
+++ src/login.java
@@ -1,8 +1,115 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
-public class login {
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+
+public class login extends JDialog{
+    private JTextField emailTF;
+    private JPasswordField passwordTF;
     private JButton CANCELButton;
     private JButton OKButton;
+    private JPanel loginPanel;
+
+    public User user;
+    public login(JFrame parent){
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(640,480));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        //setVisible(true);
+
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email=emailTF.getText();
+                String password=String.valueOf(passwordTF.getPassword());
+                System.out.println("boton ok");
+                user=getAuthenticationUser(email,password);
+
+                if (user!=null){
+                    dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(
+                            login.this,"email o password incorrectos",
+                            "intente nuevamente",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                }
+
+            }
+        });
+
+
+        CANCELButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("boton cancel");
+                dispose();
+            }
+        });
+        setVisible(true);
+    }
+
+
+    private User getAuthenticationUser(String email, String password){
+        User user =null;
+
+        final String DB_URL="jdbc:mysql://%@/farmacia?serverTimezone=UTC";
+        final String USERNAME="cualquiera";
+        final String PASSWORD="";
+
+
+        try{
+            Connection conn= DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt= conn.createStatement();
+            String sql="SELECT * FROM usuario WHERE EMAIL=? AND CONTRASEÑA=?";
+            PreparedStatement preparedStatement=conn.prepareStatement(sql);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+            System.out.println("conexion ok");
+            ResultSet resultSet=preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                user=new User();
+                user.NOMBRE=resultSet.getString("NOMBRE");
+                user.EMAIL=resultSet.getString("EMAIL");
+                user.CELULAR=resultSet.getString("CELULAR");
+                user.DIRECCION=resultSet.getString("DIRECCION");
+                user.CONTRASEÑA=resultSet.getString("CONTRASEÑA");
+            }
+
+            stmt.close();
+            conn.close();
+
+        }catch(Exception e){
+            System.out.println("error de...");
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+
+    public static void main(String[] args) {
+        login login=new login(null);
+        User user =login.user;
+
+        if(user!=null){
+            System.out.println("Autenticacion correcta:"+user.NOMBRE);
+            System.out.println("email: "+user.EMAIL);
+            //System.out.println("celular: "+user.celular);
+            System.out.println("direccion: "+user.DIRECCION);
+            System.out.println("clave: "+user.CONTRASEÑA);
+        }
+        else{
+            System.out.println("Autenticacion fallida");
+        }
+    }
 }
