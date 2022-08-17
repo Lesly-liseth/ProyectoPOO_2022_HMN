5779c87383ac571d67625e75303553883a96a9d0
Lesly <leslyherrera851@gmail.com>
2022-08-17 18:35:10 -0500

    diseño de login
    se procedio a crear el diseño del login

--- src/login.java
+++ src/login.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton CANCELButton;
+    private JButton OKButton;
 }
