06dc449a39317402ce7e3a162b7692b28b21a692
Lesly <leslyherrera851@gmail.com>
2022-08-17 18:37:12 -0500

    creacion una clase user
    se procedio a crear la clase user con variables para el login

---
+++ src/User.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class User {
+    public String NOMBRE;
+    public String EMAIL;
+    public String DIRECCION;
+    public String CELULAR;
+
+    public String CONTRASEÑA;
+}
