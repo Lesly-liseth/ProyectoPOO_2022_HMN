26414d5b3edc81cde57cfd95a97d1f06a0152a74
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-08-20 21:29:22 -0500

    Diseño
    Diseño del formulario

--- src/cajero.java
+++ src/cajero.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class cajero {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTable table1;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JButton button1;
 }
