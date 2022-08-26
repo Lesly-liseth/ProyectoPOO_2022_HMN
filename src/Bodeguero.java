cce26a35522860f03a7101de744fd7fb470114ab
Lesly <leslyherrera851@gmail.com>
2022-08-26 17:47:05 -0500

    CAMBIOS EN EL FORMULARIO BODEGUERO
    SE PROCEDIO A CAMBIAR EL FORMULARIO DE BODEGUERO EN TODO LO QUE ES EL DISEÑO

--- src/Bodeguero.java
+++ src/Bodeguero.java
@@ -5,4 +5,8 @@ public class Bodeguero {
     private JButton verButton;
     private JButton modificarButton;
     private JButton eliminarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
 }
