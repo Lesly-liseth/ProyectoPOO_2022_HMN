b5613ea7d5dc2876c7dd3b9081e60580e835b70b
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-08-25 23:05:09 -0500

    Sección cajero
    Diseño del formulario y cabecera de tabla a utilizar

--- src/cajero1.java
+++ src/cajero1.java
@@ -1,12 +1,44 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class cajero1 {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTable table1;
+public class cajero1 extends JFrame {
+    private JPanel panel;
+    private JTextField cedula;
+    private JTextField direccion;
+    private JTextField correo;
+    private JTextField nombre;
+    private JTextField telefono;
+    private JTextField fecha;
+    private JTable tabla;
+    private JTextField producto;
+    private JButton bucar;
+    private JButton agregar;
+    private JButton FINALIZARButton;
+    private JButton eliminar;
+    private JButton TOTALButton;
+    private JTextField cantidad;
+    private JButton agregarButton;
+
+    DefaultTableModel model = new DefaultTableModel();
+
+    public cajero1() {
+        setTitle("FACTURACIÓN");
+        setSize(720, 500);
+        setContentPane(panel);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+        String[] titulo = new String[]{"ID", "PRODUCTO", "DESCRIPCION", "CANTIDAD", "PRECIO", "SUBTOTAL"};
+        model.setColumnIdentifiers(titulo);
+        tabla.setModel(model);
+
+        //funcion agregar a la tabla
+    }
+
+
+    public static void main(String[] args) {
+        cajero1 cajeros = new cajero1();
+    }
 }
