bfa98148568839de165e89b2b0f889d4e8c2c76a
Lesly <leslyherrera851@gmail.com>
2022-08-28 16:03:40 -0500

    funcionalidad a los botones de la parte bodeguero
    Se le dio funcionalidad a los botones de bodeguero

--- src/Bodeguero.java
+++ src/Bodeguero.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Bodeguero {
+public class Bodeguero extends JFrame{
     private JButton ingresarButton;
     private JButton verButton;
     private JButton modificarButton;
@@ -9,4 +11,32 @@ public class Bodeguero {
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
+
+
+    public Bodeguero() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        verButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        modificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
