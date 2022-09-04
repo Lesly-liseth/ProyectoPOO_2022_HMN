8daf272dbd9755cde3a8297c40513ca76145a655
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-09-04 14:53:08 -0500

    método generar
    Se creo el método generar que permite desplegar un pdf con el detalle de la venta simulando una nota de venta. Además, se incluyo el botón de generar la nota de venta.

--- src/cajero1.java
+++ src/cajero1.java
@@ -1,7 +1,15 @@
+
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
 import java.sql.*;
 
 public class cajero1 extends JFrame {
@@ -22,6 +30,7 @@ public class cajero1 extends JFrame {
     private JButton agregar;
     private JTextField resultado;
     private JButton LIMPIARButton;
+    private JButton generar;
 
     DefaultTableModel model = new DefaultTableModel();
 
@@ -71,6 +80,12 @@ public class cajero1 extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 total();
+            }
+        });
+        generar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generar_nota();
             }
         });
     }
@@ -252,6 +267,119 @@ public class cajero1 extends JFrame {
         int fila = tabla.getSelectedRow();
         model.removeRow(fila);
     }
+
+    public void generar_nota(){
+
+        try {
+            //CREACIÓN DE DOCUMENTO
+            Document doc = new Document();
+            PdfWriter.getInstance(doc, new FileOutputStream("src/pdf/nuevo.pdf"));
+            doc.open();
+            //DATOS FARMACIA
+
+
+            String nom = "FARMACIA EL AHORRO";
+            String ruc = "1726895160001";
+            String fact = "NOTA DE VENTA";
+            String num = "022618620/0992626788";
+            String em = "farmaAhorro12@gmail.com";
+
+            Paragraph titulo = new Paragraph(nom + "\n" + ruc + "\n" + fact + "\n" + num + "\n" + em + "\n");
+            titulo.setAlignment(1);
+            doc.add(titulo);
+            doc.add(Chunk.NEWLINE);
+
+            PdfPTable tabcli = new PdfPTable(6);
+            tabcli.setWidthPercentage(100);
+            tabcli.getDefaultCell().setBorder(0);
+            PdfPCell ced = new PdfPCell(new Phrase("CÉDULA "));
+            PdfPCell nomb = new PdfPCell(new Phrase("NOMBRE "));
+            PdfPCell dir = new PdfPCell(new Phrase("DIRECCIÓN "));
+            PdfPCell corr = new PdfPCell(new Phrase("CORREO "));
+            PdfPCell tel = new PdfPCell(new Phrase("TELEFONO "));
+            PdfPCell fec = new PdfPCell(new Phrase("FECHA "));
+
+            ced.setBorder(0);
+            nomb.setBorder(0);
+            dir.setBorder(0);
+            corr.setBorder(0);
+            tel.setBorder(0);
+            fec.setBorder(0);
+
+            tabcli.addCell(ced);
+            tabcli.addCell(nomb);
+            tabcli.addCell(dir);
+            tabcli.addCell(corr);
+            tabcli.addCell(tel);
+            tabcli.addCell(fec);
+
+            tabcli.addCell(cedula.getText());
+            tabcli.addCell(nombre.getText());
+            tabcli.addCell(direccion.getText());
+            tabcli.addCell(correo.getText());
+            tabcli.addCell(telefono.getText());
+            tabcli.addCell(fecha.getText());
+
+            doc.add(tabcli);
+
+            doc.add(Chunk.NEWLINE);
+
+            PdfPTable tab = new PdfPTable(6);
+            tab.setWidthPercentage(100);
+            PdfPCell id1 = new PdfPCell(new Phrase("Id"));
+            id1.setBackgroundColor(BaseColor.CYAN.brighter());
+            PdfPCell prod = new PdfPCell(new Phrase("Producto"));
+            prod.setBackgroundColor(BaseColor.YELLOW);
+            PdfPCell desc = new PdfPCell(new Phrase("Descripcion"));
+            desc.setBackgroundColor(BaseColor.CYAN.brighter());
+            PdfPCell prec = new PdfPCell(new Phrase("Precio"));
+            prec.setBackgroundColor(BaseColor.YELLOW);
+            PdfPCell cant = new PdfPCell(new Phrase("Cantidad"));
+            cant.setBackgroundColor(BaseColor.CYAN.brighter());
+            PdfPCell subt = new PdfPCell(new Phrase("Subtotal"));
+            subt.setBackgroundColor(BaseColor.YELLOW);
+
+
+            tab.addCell(id1);
+            tab.addCell(prod);
+            tab.addCell(desc);
+            tab.addCell(prec);
+            tab.addCell(cant);
+            tab.addCell(subt);
+
+            for (int row = 0; row < tabla.getRowCount(); row++) {
+                for (int column = 0; column < tabla.getColumnCount(); column++) {
+                    tab.addCell(tabla.getValueAt(row, column).toString());
+                }
+            }
+
+            doc.add(tab);
+
+            doc.add(Chunk.NEWLINE);
+            doc.add(Chunk.NEWLINE);
+
+            //total factura
+            PdfPTable tabtot= new PdfPTable(1);
+            tabtot.setWidthPercentage(15);
+
+
+            PdfPCell tot = new PdfPCell(new Phrase("TOTAL"));
+            tot.setBackgroundColor(BaseColor.RED);
+            tot.setHorizontalAlignment(Element.ALIGN_JUSTIFIED);
+            tot.setBorder(0);
+            tabtot.addCell(tot);
+            tabtot.addCell("$"+resultado.getText());
+
+
+            doc.add(tabtot);
+            doc.close();
+
+
+        }catch (Exception ex){
+
+        }
+    }
+
 
 
     public static void main(String[] args) {
