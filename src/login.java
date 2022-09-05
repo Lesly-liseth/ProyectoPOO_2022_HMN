01581396cb3e806285e25e5bf10fd1261d70c07d
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-09-04 23:36:06 -0500

    Funcionalidad login
    Se procedió a crear login con sus correcciones en base a los roles propuestos los cuales son: cajero, administrador, bodeguero

---
+++ src/login.java
@@ -0,0 +1,107 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class login extends JFrame {
+
+    private JTextField email;
+    private JPasswordField password;
+    private JButton OK;
+    private JButton cancelar;
+    private JPanel mainPanel;
+
+    public login() {
+
+
+        setTitle("Login");
+        setSize(720, 480);
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        Connection con;
+        PreparedStatement pst;
+
+        OK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String correo = email.getText();
+                String pass = String.valueOf(password.getPassword());
+
+                String sql = "SELECT email,password,rol FROM usuarios WHERE email='"+correo+"' ";
+                try {
+                    Connection con = conectar_BDD();
+                    PreparedStatement ps  = con.prepareStatement(sql);
+                    ResultSet rs = ps.executeQuery();
+
+                    if(rs.next()) {
+
+                        String em = rs.getString("email");
+                        String p = rs.getString("password");
+                        String r = rs.getString("rol");
+
+                        if (pass.equals(p)) {
+                            if (r.equals("cajero")) {
+
+                                System.out.println("Bienvenido cajero");
+
+                            } else if (r.equals("administrador")) {
+                                System.out.println("Bienvenido admin");
+
+                            } else if(r.equals("bodeguero")){
+                                System.out.println("Bienvenido bodeguero");
+                            }
+                            else{
+                                System.out.println("USUARIO NO REGISTRADO");
+                            }
+
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "CONTRASEÑA INCORRECTA");
+                        }
+                    }
+                }
+                catch (SQLException ex){
+                    ex.printStackTrace();
+                    System.out.println("ERROR DE CONEXIÓN A LA BASE DE DATOS2");
+                }
+            }
+        });
+        cancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static Connection conectar_BDD() {
+
+        final String DB_URL = "jdbc:mysql://localhost/productos?serverTimezone=UTC";
+        final String USERNAME = "pame";
+        final String PASSWORD = "1234";
+
+        try {
+
+            Connection con = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Statement stmt = con.createStatement();
+            System.out.println("conexion exitosa");
+
+            return con;
+
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+            System.out.println("ERROR DE CONEXIÓN A LA BASE DE DATOS1");
+            return null;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        login Login = new login();
+    }
+}
